bef2fc5d1e8315f91541f4a6b6edc6e900cedd5d
heweimin <heweimin@zhejianglab.com>
2023-06-16 17:33:50 +0800

    add

---
+++ Leetcode/1494.minNumberOfSemesters.cpp
@@ -0,0 +1,46 @@
+﻿#include <string>
+#include <array>
+#include <vector>
+#include "gtest.h"
+#include <iostream>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <queue>
+#include <numeric>
+
+#include "support.h"
+
+using namespace std;
+
+namespace {
+
+class Solution {
+public:
+    int minNumberOfSemesters(int n, vector<vector<int>>& relations, int k) {
+        return 0;
+    }
+};
+
+class TestSolution :public testing::Test
+{
+public:
+    virtual void SetUp()
+    {
+    }
+    virtual void TearDown()
+    {
+    }
+};
+
+TEST_F(TestSolution, TestMain)
+{
+    vector<vector<int>> relations = {};
+    Solution s;
+    auto actual = s.minNumberOfSemesters(4, relations, 2);
+    decltype(actual) expect = 3;
+    EXPECT_EQ(expect, actual);
+}
+
+}
