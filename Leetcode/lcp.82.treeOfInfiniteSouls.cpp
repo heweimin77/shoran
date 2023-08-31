01f544680ff5789c99bf61b03b611b402b4372b7
heweimin <heweimin@zhejianglab.com>
2023-08-31 23:18:57 +0800

    lcp.82

--- Leetcode/lcp.82.treeOfInfiniteSouls.cpp
+++ Leetcode/lcp.82.treeOfInfiniteSouls.cpp
@@ -10,6 +10,7 @@
 #include <queue>
 #include <numeric>
 #include <string>
+#include <functional>
 
 #include "support.h"
 
@@ -17,8 +18,148 @@ using namespace std;
 
 namespace {
 
-// Timeout 79
 class Solution {
+public:
+    int treeOfInfiniteSouls(vector<int>& gem, int p, int target) {
+        if (p == 2 && target != 1) return 0;
+        if (p == 5 && target != 4) return 0;
+        int n = gem.size();
+        array < vector<int>, 10 > masks;
+        for (int i = 0; i < (1 << n); ++i) {
+            int x = i;
+            int cnt = 0;
+            while (x > 0) {
+                ++cnt;
+                x &= (x - 1);
+            }
+            masks[cnt].push_back(i);
+        }
+        if (p == 2 || p == 5) {
+            array<int, 512> counts;
+            fill(counts.begin(), counts.end(), 0);
+            for (int i = 0; i < n; ++i) {
+                counts[1 << i] = 1;
+            }
+            for (int cnt = 2; cnt <= n; ++cnt) {
+                for (int i = 1; i < cnt; ++i) {
+                    for (auto lm : masks[i]) {
+                        for (auto rm : masks[cnt - i]) {
+                            if (lm & rm) continue;
+                            counts[lm | rm] += counts[lm] * counts[rm];
+                        }
+                    }
+                }
+            }
+            return counts[(1 << n) - 1];
+        }
+        
+        array<long long, 134> power10;
+        array<long long, 134> ipower10;
+
+        long long r = 1;
+        for (int i = 0; i < power10.size(); ++i) {
+            power10[i] = r;
+            ipower10[i] = fast_power(r, p - 2, p);
+            r = (r * 10) % p;
+        }
+
+        array<int, 512> lens;
+        fill(lens.begin(), lens.end(), 0);
+        for (int i = 0; i < n; ++i) {
+            lens[1 << i] = to_string(gem[i]).size() + 2;
+        }
+        for (int cnt = 2; cnt <= n; ++cnt) {
+            for (auto lm : masks[1]) {
+                for (auto rm : masks[cnt - 1]) {
+                    if (lm & rm) continue;
+                    lens[lm | rm] = lens[lm] + lens[rm] + 2;
+                }
+            }
+        }
+
+        array<unordered_map<long long, int>, 512> results;
+        for (int i = 0; i < n; ++i) {
+            int len = to_string(gem[i]).size();
+            long long r = power10[len + 1] + 10LL * gem[i] + 9;
+            results[1 << i][r % p] = 1;
+        }
+
+        if (n == 1) {
+            return results[1][target];
+        }
+        int limit = min(6, n);
+        for (int cnt = 2; cnt <= limit; ++cnt) {
+            for (int i = 1; i < cnt; ++i) {
+                for (auto lm : masks[i]) {
+                    for (auto rm : masks[cnt - i]) {
+                        if (lm & rm) continue;
+                        auto& result = results[lm | rm];
+                        int llen = lens[lm];
+                        int rlen = lens[rm];
+                        for (auto lr : results[lm]) {
+                            for (auto rr : results[rm]) {
+                                long long r = power10[llen + rlen + 1] + lr.first * power10[rlen + 1] + rr.first * 10 + 9;
+                                result[r % p] += lr.second * rr.second;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        if (n <= 6) {
+            return results[(1 << n) - 1][target];
+        }
+
+        function<int(int, int, int)> dfs = [&](int cnt, int flag, int value){
+            if (cnt <= 6) {
+                auto it = results[flag].find(value);
+                return it == results[flag].end() ? 0 : it->second;
+            }
+            auto it = results[flag].find(value);
+            if (it != results[flag].end()) return it->second;
+            int count = 0;
+            for (int i = 1, j = cnt - i; i <= j; ++i, --j) {
+                for (auto lm : masks[i]) {
+                    if ((lm | flag) != flag) continue;
+                    auto gm = lm ^ flag;
+                    for (auto& lr : results[lm]) {
+                        // lr & gm
+                        long long part = (power10[lens[lm] + lens[gm] + 1] + lr.first * power10[lens[gm] + 1] + 9) % p;
+                        long long x = (value + p - part) * ipower10[1] % p;
+                        count += lr.second * dfs(j, gm, x);
+
+                        if (i < j) {
+                            // gm & lr
+                            part = (power10[lens[lm] + lens[gm] + 1] + lr.first * power10[1] + 9) % p;
+                            x = (value + p - part) * ipower10[1 + lens[lm]] % p;
+                            count += lr.second * dfs(j, gm, x);
+                        }
+                    }
+
+                }
+            }
+            results[flag][value] = count;
+            return count;
+        };
+        return dfs(n, (1 << n) - 1, target);
+    }
+
+    long long fast_power(long long base, long long n, long long module) {
+        long long result = 1;
+        int mask = 1;
+        for (int mask = 1; mask <= n; mask <<= 1) {
+            if (mask & n) {
+                result *= base;
+                result %= module;
+            }
+            base *= base;
+            base %= module;
+        }
+        return result;
+    }
+};
+
+class Solution3Timeout79 {
 public:
     int treeOfInfiniteSouls(vector<int>& gem, int p, int target) {
         if (p == 2 && target != 1) return 0;
@@ -190,7 +331,7 @@ private:
     array < vector<int>, 10 > masks;
 };
 
-class Solution3Timeout82 {
+class Solution1Timeout82 {
 public:
     int treeOfInfiniteSouls(vector<int>& gem, int p, int target) {
         long long r = 1;
